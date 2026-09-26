e7f5c17c96b066f335d4762a24541d524547be7b
Innocent Elufere <122732911+Elufere@users.noreply.github.com>
2026-09-26 09:30:23 +0100

    Initialize captured observations

--- backend/test_pm.py
+++ backend/test_pm.py
@@ -74,7 +74,7 @@ def new_session():
     state = AgentState(session_id=str(uuid4()), messages=[HumanMessage(content=idea, id=str(uuid4()))],
         raw_idea=idea, prd_contract=None, pm_is_complete=False,
         discovery_scope=DiscoveryScope.USER_APP, turn_count=0, awaiting_confirmation=False,
-        discovered_knowledge=[], superseded_knowledge=[], product_concepts=[],
+        discovered_knowledge=[], superseded_knowledge=[], product_concepts=[], captured_observations=[],
         model_implications=[],
         thread_planning_enabled=True, discovery_threads={}, active_discovery_thread=None, thread_frontier=None,
         thread_relevant_requirement_ids=[],
